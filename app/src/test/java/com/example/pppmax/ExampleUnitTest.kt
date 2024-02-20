a62825f5da15937aad6538aef51ceb65c1ecec2e
arthur13stalberg09silva07 <arthur.stalberg@germianre.org.br>
2024-02-20 15:53:06 -0300

    fazendo teste unitário

--- app/src/test/java/com/example/pppmax/ExampleUnitTest.kt
+++ app/src/test/java/com/example/pppmax/ExampleUnitTest.kt
@@ -1,6 +1,13 @@
 package com.example.pppmax
 
-import org.junit.Assert.assertEquals
+import com.example.pppmax.data.repository.MovieRepositoryImpl
+import com.example.pppmax.domain.model.Movie
+import com.example.teste2api.MovieApi
+import io.mockk.coEvery
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 /**
@@ -9,8 +16,24 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+   fun `MovieRepositoryImpl getMovie retorna um objeto do tipo movie`() = runBlocking {
+        val movie = Movie(
+            id = "111",
+            title = "Scarface",
+            overview = "Após receber residência permanente nos Estados Unidos em troca do assassinato de um oficial do governo cubano, Tony Montana se torna o chefe do tráfico de drogas em Miami. Matando qualquer um que entre em seu caminho, Tony eventualmente se torna o maior traficante da Florída, controlando quase toda a cocaína que entra em Miami.Porém, a pressão da polícia, as guerras com cartéis colombianos e sua própia paranoia servem para alimentar as chamas de sua eventual queda.",
+            backdrop_path = "foto"
+            )
+        val api: MovieApi= mockk()
+        coEvery { api.getMovie("111") } coAnswers { Movie(
+            id="111",
+            title="Scarface",
+            overview = "Após receber residência permanente nos Estados Unidos em troca do assassinato de um oficial do governo cubano, Tony Montana se torna o chefe do tráfico de drogas em Miami. Matando qualquer um que entre em seu caminho, Tony eventualmente se torna o maior traficante da Florída, controlando quase toda a cocaína que entra em Miami.Porém, a pressão da polícia, as guerras com cartéis colombianos e sua própia paranoia servem para alimentar as chamas de sua eventual queda.",
+            backdrop_path = "foto"
+        )
+        }
+
+        assertEquals(MovieRepositoryImpl(api).getMovieById("111").first(), movie)
     }
 }
