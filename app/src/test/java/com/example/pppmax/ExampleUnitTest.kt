0354d3446409dfdae2b9b470fbada18180dd3dc9
arthur13stalberg09silva07 <arthur.stalberg@germianre.org.br>
2024-02-21 15:39:48 -0300

    teste unitario dando certo

--- app/src/test/java/com/example/pppmax/ExampleUnitTest.kt
+++ app/src/test/java/com/example/pppmax/ExampleUnitTest.kt
@@ -27,7 +27,7 @@ class ExampleUnitTest {
             backdrop_path = "foto"
             )
         val api: MovieApi= mockk()
-        coEvery { api.getMovie("Scarface") } coAnswers {MovieResponse( listOf<Movie>(Movie(
+        coEvery { api.getMovie("111") } coAnswers {MovieResponse( listOf<Movie>(Movie(
             id="111",
             title="Scarface",
             overview = "Após receber residência permanente nos Estados Unidos em troca do assassinato de um oficial do governo cubano, Tony Montana se torna o chefe do tráfico de drogas em Miami. Matando qualquer um que entre em seu caminho, Tony eventualmente se torna o maior traficante da Florída, controlando quase toda a cocaína que entra em Miami.Porém, a pressão da polícia, as guerras com cartéis colombianos e sua própia paranoia servem para alimentar as chamas de sua eventual queda.",
